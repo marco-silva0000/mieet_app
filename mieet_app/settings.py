d2fa9654e9bfea2f945835382a07658ff2066372
msilva <msilva@synopsys.com>
2019-10-17 00:07:05 +0100

    small fixes for deployment

--- mieet_app/settings.py
+++ mieet_app/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.marco-silva.com', '139.162.169.46']
 
 
 # Application definition
